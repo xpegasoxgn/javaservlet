22e02112536dec18e0a98fbeac82eb17a20bf18b
gnavia <ginavia12@gmail.com>
2026-02-20 22:03:10 -0400

    agregadondo servicios HTTP

---
+++ src/main/java/com/curso/UsuarioServlet.java
@@ -0,0 +1,57 @@
+package com.curso;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/usuario")
+public class UsuarioServlet extends HttpServlet {
+    private List <String> usuarios= new ArrayList<>();
+
+    //GET
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Lista de Usuarios</h1>");
+        for (String usuario : usuarios) {
+            response.getWriter().println("<p>" + usuario + "</p>");
+        }
+    }
+
+     //POST
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String nombre = request.getParameter("nombre");
+        if (nombre != null && !nombre.trim().isEmpty()) {
+            usuarios.add(nombre);
+        }
+        response.sendRedirect("usuario");
+
+    
+    }
+    //PUT
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String nombre = request.getParameter("nombre");
+        String nuevoNombre = request.getParameter("nuevoNombre");
+        if (nombre != null && nuevoNombre != null && usuarios.contains(nombre)) {
+            int index = usuarios.indexOf(nombre);
+            usuarios.set(index, nuevoNombre);
+        }
+        response.sendRedirect("usuario");
+    }
+    //DELETE
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String nombre = request.getParameter("nombre");
+        if (nombre != null && usuarios.contains(nombre)) {
+            usuarios.remove(nombre);
+        }
+        response.sendRedirect("usuario");
+    }
+
+    
+}
